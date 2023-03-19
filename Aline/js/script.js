b0810a5b5fd974d0fba0698e1379123676a0937d
MrMagalhaes <127852558+MrMagalhaes@users.noreply.github.com>
2023-03-19 17:05:00 -0300

    Add files via upload

---
+++ Aline/js/script.js
@@ -0,0 +1,7 @@
+for (let index = 9; index < array.length; index++) {
+    fuction
+} trocar();
+	
+let minhaimagem = document.getElementById("imagens"),
+let, Math,trunc(Math.random()*9);
+minhaimagem.src - "img/" + ".png";

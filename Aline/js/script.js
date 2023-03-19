32047cbf70929c1f93834c5b894abc537d62d7ea
MrMagalhaes <127852558+MrMagalhaes@users.noreply.github.com>
2023-03-19 17:08:04 -0300

    Delete Aline directory

--- Aline/js/script.js
+++
@@ -1,7 +0,0 @@
-for (let index = 9; index < array.length; index++) {
-    fuction
-} trocar();
-	
-let minhaimagem = document.getElementById("imagens"),
-let, Math,trunc(Math.random()*9);
-minhaimagem.src - "img/" + ".png";
